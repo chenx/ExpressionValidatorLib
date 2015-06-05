83bfd762e99eabcf61e8eaa6f6da032189cf61e4
Tom Chen <txchen@gmail.com>
2015-06-04 18:25:07 -1000

    Update ExprValidator.cs

--- ExpressionValidatorLib/ExpressionValidatorLib/ExprValidator.cs
+++ ExpressionValidatorLib/ExpressionValidatorLib/ExprValidator.cs
@@ -14,7 +14,7 @@ namespace ExpressionValidatorLib
     /// num is of format a or a.b, cannot be a. or .b,
     /// var type1 is of format id,
     /// var type2 is of format id.id,
-    /// ID is of format [a-zA-Z][a-zA-Z0-9_]+.
+    /// id is of format [a-zA-Z][a-zA-Z0-9_]+.
     /// 
     /// Author: X. C.
     /// Created on: 3/12/2015
